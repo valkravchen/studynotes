a45bad0979ecb55011b1393ac20e51ab2656cb84
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-02-16 19:46:24 +0300

    Создан метод update(обновить заметку)

--- src/main/java/com/dobrynya/studynotes/service/NoteService.java
+++ src/main/java/com/dobrynya/studynotes/service/NoteService.java
@@ -34,4 +34,12 @@ public class NoteService {
         }
         noteRepository.deleteById(id);
     }
+
+    public Note update(Long id, Note updateNote) {
+        Note existing = findById(id);
+        existing.setTitle(updateNote.getTitle());
+        existing.setContent(updateNote.getContent());
+        existing.setType(updateNote.getType());
+        return noteRepository.save(existing);
+    }
 }
