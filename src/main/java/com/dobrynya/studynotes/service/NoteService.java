c2712f79dbf5bba16854cca7fa769d61bf700bb0
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-02-17 08:41:48 +0300

    В сервис добавлен приватный метод findNoteById для получения Entity

--- src/main/java/com/dobrynya/studynotes/service/NoteService.java
+++ src/main/java/com/dobrynya/studynotes/service/NoteService.java
@@ -44,11 +44,16 @@ public class NoteService {
         noteRepository.deleteById(id);
     }
 
-    public Note update(Long id, Note updateNote) {
-        Note existing = findById(id);
-        existing.setTitle(updateNote.getTitle());
-        existing.setContent(updateNote.getContent());
-        existing.setType(updateNote.getType());
-        return noteRepository.save(existing);
+//    public Note update(Long id, Note updateNote) {
+//        Note existing = findById(id);
+//        existing.setTitle(updateNote.getTitle());
+//        existing.setContent(updateNote.getContent());
+//        existing.setType(updateNote.getType());
+//        return noteRepository.save(existing);
+//    }
+
+    private Note findNoteById(Long id) {
+        return noteRepository.findById(id)
+                .orElseThrow(() -> new NoteNotFoundException(id));
     }
 }
