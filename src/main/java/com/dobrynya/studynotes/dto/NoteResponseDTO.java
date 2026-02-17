0bc1b651813a6fa169357f00c1364bb61b65639d
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-02-17 07:48:33 +0300

    Создан NoteResponseDTO

---
+++ src/main/java/com/dobrynya/studynotes/dto/NoteResponseDTO.java
@@ -0,0 +1,63 @@
+package com.dobrynya.studynotes.dto;
+
+import java.time.LocalDateTime;
+
+public class NoteResponseDTO {
+    private Long id;
+    private String title;
+    private String content;
+    private String type;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public NoteResponseDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
