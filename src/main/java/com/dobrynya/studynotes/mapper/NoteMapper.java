fea4e78a8ad0c82cf5d1b38f77d26e2fa80ac8df
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-02-17 07:57:02 +0300

    Создан метод toEntity — DTO → Entity (в NoteMapper)

--- src/main/java/com/dobrynya/studynotes/mapper/NoteMapper.java
+++ src/main/java/com/dobrynya/studynotes/mapper/NoteMapper.java
@@ -1,7 +1,20 @@
 package com.dobrynya.studynotes.mapper;
 
+import com.dobrynya.studynotes.dto.NoteCreateDTO;
+import com.dobrynya.studynotes.model.Note;
+import com.dobrynya.studynotes.model.NoteType;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NoteMapper {
+    public Note toEntity(NoteCreateDTO dto) {
+        Note note = new Note();
+        note.setTitle(dto.getTitle());
+        note.setContent(dto.getContent());
+
+        if (dto.getType() != null && !dto.getType().isBlank()) {
+            note.setType(NoteType.valueOf(dto.getType().toUpperCase()));
+        }
+        return note;
+    }
 }
