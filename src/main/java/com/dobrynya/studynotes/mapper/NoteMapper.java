0c538db1f375f904984769852dfc9614c9482143
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-02-17 08:04:05 +0300

    Создан метод toResponseDTO — Entity → DTO (в NoteMapper)

--- src/main/java/com/dobrynya/studynotes/mapper/NoteMapper.java
+++ src/main/java/com/dobrynya/studynotes/mapper/NoteMapper.java
@@ -1,6 +1,7 @@
 package com.dobrynya.studynotes.mapper;
 
 import com.dobrynya.studynotes.dto.NoteCreateDTO;
+import com.dobrynya.studynotes.dto.NoteResponseDTO;
 import com.dobrynya.studynotes.model.Note;
 import com.dobrynya.studynotes.model.NoteType;
 import org.springframework.stereotype.Component;
@@ -16,5 +17,19 @@ public class NoteMapper {
             note.setType(NoteType.valueOf(dto.getType().toUpperCase()));
         }
         return note;
+    }
+
+    public NoteResponseDTO toResponseDTO(Note note) {
+        NoteResponseDTO dto = new NoteResponseDTO();
+        dto.setId(note.getId());
+        dto.setTitle(note.getTitle());
+        dto.setContent(note.getContent());
+        dto.setCreatedAt(note.getCreatedAt());
+        dto.setUpdatedAt(note.getUpdatedAt());
+
+        if (note.getType() != null) {
+            dto.setType(note.getType().name().toUpperCase());
+        }
+        return dto;
     }
 }
