8be19bd8cd1286d7f90a46d103f0b8152ed1814f
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-02-17 14:10:11 +0300

    В контроллере NoteController создан метод findAll

--- src/main/java/com/dobrynya/studynotes/controller/NoteController.java
+++ src/main/java/com/dobrynya/studynotes/controller/NoteController.java
@@ -1,8 +1,12 @@
 package com.dobrynya.studynotes.controller;
 
+import com.dobrynya.studynotes.dto.NoteResponseDTO;
 import com.dobrynya.studynotes.service.NoteService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/notes")
@@ -11,5 +15,10 @@ public class NoteController {
 
     public NoteController(NoteService noteService) {
         this.noteService = noteService;
+    }
+
+    @GetMapping
+    public List<NoteResponseDTO> findAll() {
+        return noteService.findAll();
     }
 }
