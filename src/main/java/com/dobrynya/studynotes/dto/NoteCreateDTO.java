52da22aa96284724b9bea048b53ef0c5eaf38f41
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-02-17 07:45:15 +0300

    Создан

---
+++ src/main/java/com/dobrynya/studynotes/dto/NoteCreateDTO.java
@@ -0,0 +1,34 @@
+package com.dobrynya.studynotes.dto;
+
+public class NoteCreateDTO {
+    private String title;
+    private String content;
+    private String type;
+
+    public NoteCreateDTO() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
