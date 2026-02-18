04162b1ff48dd5902dbbd6f93922af4f5cd5331a
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-02-18 13:23:43 +0300

    Добавлен метод findByType в репозиторий NoteRepository

--- src/main/java/com/dobrynya/studynotes/repository/NoteRepository.java
+++ src/main/java/com/dobrynya/studynotes/repository/NoteRepository.java
@@ -1,9 +1,13 @@
 package com.dobrynya.studynotes.repository;
 
 import com.dobrynya.studynotes.model.Note;
+import com.dobrynya.studynotes.model.NoteType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
+    List<Note> findByType(NoteType type);
 }
