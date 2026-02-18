91d8d3662774fad7174dacb54fcc2b939f602d20
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-02-18 08:46:40 +0300

    Добавлен метод handleNoteNotFound(обработчик NoteNotFoundException) в классе GlobalExceptionHandler

--- src/main/java/com/dobrynya/studynotes/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dobrynya/studynotes/exception/GlobalExceptionHandler.java
@@ -1,7 +1,22 @@
 package com.dobrynya.studynotes.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoteNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNoteNotFound(NoteNotFoundException ex) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
 }
