1083e265c9ca06142c8067c53f32ac25dc39e61e
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-02-19 09:25:36 +0300

    Добавлен метод listNotes в WebController

--- src/main/java/com/dobrynya/studynotes/controller/WebController.java
+++ src/main/java/com/dobrynya/studynotes/controller/WebController.java
@@ -1,11 +1,13 @@
 package com.dobrynya.studynotes.controller;
 
+import com.dobrynya.studynotes.dto.NoteResponseDTO;
 import com.dobrynya.studynotes.service.NoteService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Controller
 public class WebController {
@@ -21,5 +23,12 @@ public class WebController {
         model.addAttribute("noteCount", noteService.findAll(null).size());
         model.addAttribute("currentDate", LocalDateTime.now());
         return "index";
+    }
+
+    @GetMapping("/notes")
+    public String listNotes(Model model) {
+        List<NoteResponseDTO> notes = noteService.findAll(null);
+        model.addAttribute("notes", notes);
+        return "list-notes";
     }
 }
