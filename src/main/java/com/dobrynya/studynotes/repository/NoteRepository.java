3f88a1805be362b44bd97b06ccf17a90555482ca
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-02-22 18:34:38 +0300

    3

--- src/main/java/com/dobrynya/studynotes/repository/NoteRepository.java
+++ src/main/java/com/dobrynya/studynotes/repository/NoteRepository.java
@@ -10,6 +10,5 @@ import java.util.List;
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
     List<Note> findByType(NoteType type);
-
     List<Note> findByTitleContainingIgnoreCase(String title);
 }
