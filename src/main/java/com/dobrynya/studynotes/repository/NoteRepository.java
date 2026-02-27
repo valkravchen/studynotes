40243439909dc54ba47b85770b11930039ce7464
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-02-27 20:49:19 +0300

    Добавлен метод fullTextSearch(String query) в NoteRepository. Цель: нативный SQL-запрос для полнотекстового поиска по заголовку и содержимому заметки

--- src/main/java/com/dobrynya/studynotes/repository/NoteRepository.java
+++ src/main/java/com/dobrynya/studynotes/repository/NoteRepository.java
@@ -3,7 +3,9 @@ package com.dobrynya.studynotes.repository;
 import com.dobrynya.studynotes.model.Note;
 import com.dobrynya.studynotes.model.NoteType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -16,4 +18,11 @@ public interface NoteRepository extends JpaRepository<Note, Long> {
     boolean existsByTitle(String title);
 
     List<Note> findAllByOrderByTitleAsc();
+
+    @Query(value = """
+            SELECT * FROM notes
+            WHERE to_tsvector('simple', title) || to_tsvector('simple', COALESCE(content, '') )
+                        @@ plainto_tsquery('simple', :query)
+            """, nativeQuery = true)
+    List<Note> fullTextSearch(@Param("query") String query);
 }
