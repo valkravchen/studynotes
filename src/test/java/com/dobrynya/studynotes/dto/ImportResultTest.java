1e2cca11b8edf083e62728253e97072007d904a5
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-02-28 13:25:10 +0300

    Удалён лишний импорт

--- src/test/java/com/dobrynya/studynotes/dto/ImportResultTest.java
+++ src/test/java/com/dobrynya/studynotes/dto/ImportResultTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
